025d09263bb2b76d120d5550ba02389249f65119
James Carr <james.r.carr@gmail.com>
2024-07-25 08:34:04 -0500

    elixir in action work so far

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,33 +1,50 @@
 defmodule Todo.Server do
-  use Agent, restart: temporary
+  use GenServer
+  @timeout :timer.seconds(10)
 
   def start_link(name) do
-    Agent.start_link(
-      fn ->
-        IO.puts("Starting todo-server for #{name}")
-        {name, Todo.Database.get(name) || Todo.List.new()}
-      end,
-      name: via_tuple(name)
-    )
+    GenServer.start_link(Todo.Server, name)
   end
 
   def add_entry(todo_server, new_entry) do
-    Agent.cast(todo_server, fn {name, todo_list} ->
-      new_list = Todo.List.add_entry(todo_list, new_entry)
-      Todo.Database.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    Agent.get(
-      todo_server,
-      fn {_name, todo_list} -> Todo.List.entries(todo_list, date) end
-    )
+    GenServer.call(todo_server, {:entries, date})
   end
 
-  defp via_tuple(name) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
+  @impl GenServer
+  def init(name) do
+    IO.puts("Starting to-do server for #{name}.")
+    {:ok, {name, nil}, {:continue, :init}}
   end
 
+  @impl GenServer
+  def handle_continue(:init, {name, nil}) do
+    todo_list = Todo.Database.get(name) || Todo.List.new()
+    {:noreply, {name, todo_list}, @timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}, @timeout}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {name, todo_list, @timeout}
+    }
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping todo server for #{name}")
+    {:stop, :normal, {name, todo_list}}
+  end
 end
